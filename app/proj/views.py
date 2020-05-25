8a758929347ac91ecc7cfe1252d6db3e9c267dde
dmitrijgulacenkov <pingvin1959@yandex.ru>
2020-05-25 23:08:47 +0300

    deleted get data from db (because we need only actual info)

--- app/proj/views.py
+++ app/proj/views.py
@@ -14,30 +14,30 @@ class GetSearchGUIDView(APIView):
          first company(i can parse all if u want)
         """
 
-        query = SearchQuery.get_by_search_text(company_name)  # get from db
-        if (query == []):
-            # get Guid form fedres site
-            raw_companys_list = SearchQuery.get_companys_by_search_text(company_name)
-            if (raw_companys_list == []):
-                return Response([])
-            company_raw = raw_companys_list[0]
-            company = Company(company_raw["guid"], company_raw["ogrn"], company_raw["inn"], company_raw["name"],
-                              company_raw["address"], company_raw["status"],
-                              company_raw["statusDate"])
-            company.save()
-            query = SearchQuery(company_name, company.guid)
-            return Response([query.serialize_and_save()])
-        return Response(query)
+        # query = SearchQuery.get_by_search_text(company_name)  # get from db
+        # if (query == []):
+        
+        # get Guid form fedres site
+        raw_companys_list = SearchQuery.get_companys_by_search_text(company_name)
+        if (raw_companys_list == []):
+            return Response([])
+        company_raw = raw_companys_list[0]
+        company = Company(company_raw["guid"], company_raw["ogrn"], company_raw["inn"], company_raw["name"],
+                          company_raw["address"], company_raw["status"],
+                          company_raw["statusDate"])
+        company.save()
+        query = SearchQuery(company_name, company.guid)
+        return Response([query.serialize_and_save()])
+        # return Response(query)
 
 
 class GetMessagesByGUIDView(APIView):
     def get(self, request):
         company_guid = request.data["company_guid"]
         searching_by = request.data["searching_by"]
-        messages = Message.get_messages_from_db(company_guid, searching_by)
-        if (messages == []):
-            # Get messages from fedres site
-            messages = Company.get_messages(company_guid, searching_by)
+        # messages = Message.get_messages_from_db(company_guid, searching_by) # get messages from db
 
-            return Response(Message.serialize(messages))
-        return Response(messages)
+        # Get messages from fedres site
+        messages = Company.get_messages(company_guid, searching_by)
+
+        return Response(Message.serialize(messages))
